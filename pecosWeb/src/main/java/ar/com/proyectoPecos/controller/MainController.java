b13c20da2775e5a1250edd38a3067e3ca7bce85b
anacnazzo <anacnazzo@gmail.com>
2018-11-22 19:44:48 -0300

    security-3
    aplicación de layout

--- pecosWeb/src/main/java/ar/com/proyectoPecos/controller/MainController.java
+++ pecosWeb/src/main/java/ar/com/proyectoPecos/controller/MainController.java
@@ -1,34 +1,38 @@
 package ar.com.proyectoPecos.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ar.com.proyectoPecos.model.Colaborador;
-import ar.com.proyectoPecos.service.impl.ColaboradorService;
 
 @Controller
 public class MainController {
 	
-	@Autowired
-	private ColaboradorService colaboradorService;
-
+	
 	@RequestMapping("/home")
     public String home() {
-		Colaborador colaborador = new Colaborador();
-		colaborador.setEmail("anacnazzo@gmail.com");
-		colaborador.setPassword("ana123");
-		colaboradorService.save(colaborador);
+		
         return "home";
     }
 	
 	@RequestMapping("/")
     public String main() {
-		Colaborador colaborador = new Colaborador();
-		colaborador.setEmail("anacnazzo@gmail.com");
-		colaborador.setPassword("ana123");
-		colaboradorService.save(colaborador);
+		
         return "home";
+    }
+	
+	@RequestMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+
+	@RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+	@RequestMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
     }
 	
 }
